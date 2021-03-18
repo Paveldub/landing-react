8e49c803373d972d09e476e5c4f0e1d2e317c066
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-03-18 19:08:19 +0200

    random port server

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const path = require('path');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const portFinderSync = require("portfinder-sync");
 
-let mode = 'development';
-let target = 'web';
+let mode = "development";
+let target = "web";
 
 const plugins = [
   new CleanWebpackPlugin(),
   new MiniCssExtractPlugin(),
   new HtmlWebpackPlugin({
-    template: './src/index.html',
+    template: "./src/index.html",
   }),
 ];
 
-if (process.env.NODE_ENV === 'production') {
-  mode = 'production';
-  target = 'browserslist';
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
 } else {
   plugins.push(new ReactRefreshWebpackPlugin());
 }
@@ -26,27 +27,27 @@ module.exports = {
   mode: mode,
   target: target,
 
-  entry: './src/index.js',
+  entry: "./src/index.js",
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]',
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
   },
 
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset', // asset --> if small img convert to base64, if big copied as default img/// /inline --> to base64/// /resource --> to default images/img
+        type: "asset", // asset --> if small img convert to base64, if big copied as default img/// /inline --> to base64/// /resource --> to default images/img
       },
       {
         test: /\.mp4$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'video',
+              name: "[name].[ext]",
+              outputPath: "video",
             },
           },
         ],
@@ -56,24 +57,24 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: { publicPath: '' },
+            options: { publicPath: "" },
           },
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
       },
       {
         test: /\.(woff|woff2)$/,
         use: {
-          loader: 'url-loader', //convert in prod to base64
+          loader: "url-loader", //convert in prod to base64
         },
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
@@ -82,12 +83,13 @@ module.exports = {
   plugins: plugins,
 
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
 
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
-    contentBase: './dist',
+    port: portFinderSync.getPort(3000),
+    contentBase: "./dist",
     hot: true,
   },
 };
